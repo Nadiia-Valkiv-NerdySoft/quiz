edb78fc2a85405a2b413ba9e64150d20e18c316f
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2024-11-04 12:02:35 +0200

    feat: create ui-header component

---
+++ src/app/shared/ui-kit/ui-header/ui-header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'quiz-ui-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './ui-header.component.html',
+  styleUrl: './ui-header.component.scss',
+})
+export class UiHeaderComponent {
+  navItems = [
+    { label: 'Home', link: '/home' },
+    { label: 'Catalog', link: '/catalog' },
+    { label: 'Item', link: '/item' },
+    { label: 'Item', link: '/item' },
+    { label: 'Item', link: '/item' },
+  ];
+}
