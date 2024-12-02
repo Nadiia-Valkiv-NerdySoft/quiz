53c0180b711b5689e6c12ab7de198b37437fa9a5
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2024-12-02 11:04:11 +0200

    feat: improve styles

--- src/app/shared/ui-kit/ui-question-card/ui-question-card.component.ts
+++ src/app/shared/ui-kit/ui-question-card/ui-question-card.component.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
 import { AVATAR_PATHS } from '../ui-quiz-card/ui-quiz-card.constants';
-import { NgFor } from '@angular/common';
 import { UiButtonComponent } from '../ui-button/ui-button.component';
 import { SvgIconComponent } from 'angular-svg-icon';
 
 const MOCK_OPTIONS = [
-  { id: 'papper-ladies', label: 'Papper Ladies' },
-  { id: 'spice-girls', label: 'Spice Girls', checked: true },
-  { id: 'jalapenia-gaals', label: 'Jalapenia Gaals' },
-  { id: 'little-mix', label: 'Little Mix' },
+  { id: 'papper', label: 'Papper Ladies' },
+  { id: 'spice', label: 'Spice Girls', checked: true },
+  { id: 'jalapenia', label: 'Jalapenia Gaals' },
+  { id: 'little', label: 'Little Mix' },
 ];
 
 @Component({
   selector: 'quiz-ui-question-card',
   standalone: true,
-  imports: [ NgFor, UiButtonComponent, SvgIconComponent ],
+  imports: [ UiButtonComponent, SvgIconComponent ],
   templateUrl: './ui-question-card.component.html',
 })
 export class UiQuestionCardComponent {
   avatarPath = AVATAR_PATHS.PROFILE_1;
 
   mockOptions = MOCK_OPTIONS;
-  radioGroupName = 'girls';
 }
