10bc2c00a80f026eb78e5f29e852f5857d2ab6e3
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2024-12-18 16:02:51 +0200

    feat: add statistic page layout

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UiButtonComponent } from '../../shared/ui-kit/ui-button/ui-button.component';
 
 @Component({
   selector: 'quiz-statistics',
   standalone: true,
-  imports: [],
+  imports: [UiButtonComponent],
   templateUrl: './statistics.component.html',
 })
 export class StatisticsComponent {}
