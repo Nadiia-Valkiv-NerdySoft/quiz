90c48a9edb08981633baa50b61e065830e69cc26
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2024-12-24 14:27:22 +0200

    feat: install jest

--- src/app/core/services/statistics.service.spec.ts
+++ src/app/core/services/statistics.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StatisticsService } from './statistics.service';
+import { INITIAL_STATISTIC } from '../../shared/models/statistic.model';
 
 describe('StatisticsService', () => {
   let service: StatisticsService;
+  let localStorageMock: { getItem: jest.Mock };
 
   beforeEach(() => {
+    localStorageMock = {
+      getItem: jest.fn(),
+    };
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock,
+    });
+
     TestBed.configureTestingModule({});
     service = TestBed.inject(StatisticsService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return INITIAL_STATISTIC when localStorage is empty', () => {
+    localStorageMock.getItem.mockReturnValue(null);
+
+    const result = service.getStatistic();
+
+    expect(result).toEqual(INITIAL_STATISTIC);
   });
 });
