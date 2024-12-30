71940d3302b90bbaebf68b9d332b2f7578a0aa43
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2024-12-30 21:35:04 +0200

    feat:add const for feedback

--- src/app/shared/ui-kit/ui-quiz-feedback/ui-quiz-feedback.component.ts
+++ src/app/shared/ui-kit/ui-quiz-feedback/ui-quiz-feedback.component.ts
@@ -1,6 +1,10 @@
 import { Component, input } from '@angular/core';
 import { LastQuizData } from '../../models/last-quiz-data.model';
-import { TimeFormatPipe } from '../../../pages/statistics/time-format.pipe';
+import { TimeFormatPipe } from './time-format.pipe';
+import {
+  QUIZ_FEEDBACK_Default_MESSAGE,
+  QUIZ_FEEDBACK_MESSAGES,
+} from '../../constants/quiz-feedback-messages';
 
 @Component({
   selector: 'quiz-ui-quiz-feedback',
@@ -19,21 +23,11 @@ export class QuizFeedbackComponent {
 
   getFeedbackMessage(): string {
     const percentage = this.getScorePercentage();
-    if (percentage === 0) {
-      return 'Do not worry! You can always try again!';
-    }
-    if (percentage < 33) {
-      return 'Do not give up! Keep practicing, and you will see improvement.';
-    }
-    if (percentage < 66) {
-      return 'Good try! Why not have another go? You might get a bigger score!';
-    }
-    if (percentage < 100) {
-      return 'Outstanding performance! You have demonstrated excellent understanding of the material.';
-    }
-    if (percentage === 100) {
-      return 'Absolutely brilliant!';
-    }
-    return '';
+
+    const feedback = QUIZ_FEEDBACK_MESSAGES.find(
+      item => percentage <= item.maxPercentage,
+    );
+
+    return feedback?.message || QUIZ_FEEDBACK_Default_MESSAGE;
   }
 }
