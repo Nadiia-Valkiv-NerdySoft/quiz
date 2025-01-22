2901ac9663c6b5583224265cb5e7aee0da770132
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2025-01-22 12:51:32 +0200

    refactor: change number of question for quizzes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   categoriesUrl: 'https://opentdb.com/api_category.php',
   numberOfQuizCategories: 10,
-  minNumberOfQuestion: 2,
-  maxNumberOfQuestion: 4,
+  minNumberOfQuestion: 5,
+  maxNumberOfQuestion: 10,
   categoriesApiErrorMessage:
     'Can not load categories right now. Please reload the page.',
 };
