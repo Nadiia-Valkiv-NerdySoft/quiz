7dceb5b735d71253b78384872e3e3a39600fb549
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2025-01-22 12:59:01 +0200

    change number of questions for quiz

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   categoriesUrl: 'https://opentdb.com/api_category.php',
   questionUrl: 'https://opentdb.com/api.php',
   numberOfQuizCategories: 10,
-  minNumberOfQuestion: 2,
-  maxNumberOfQuestion: 4,
+  minNumberOfQuestion: 5,
+  maxNumberOfQuestion: 10,
   apiErrorMessage: 'Can not load data right now. Please try again later.',
 };
