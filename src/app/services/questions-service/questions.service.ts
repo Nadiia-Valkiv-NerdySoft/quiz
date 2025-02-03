4728ab7c3c183abe10e59f4b813dec0ae74f2b67
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2025-02-03 13:16:51 +0200

    feat: shuffle question qnswers

--- src/app/services/questions-service/questions.service.ts
+++ src/app/services/questions-service/questions.service.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../api-service/api.service';
 import { ErrorHandlerService } from '../error-handler-service/error-handler.service';
 import { QuestionApiResponse } from '../../shared/models/question-api-response.model';
 import { Question } from '../../shared/models/question.model';
+import { RandomizationService } from '../randomization-service/randomization.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,20 +12,23 @@ import { Question } from '../../shared/models/question.model';
 export class QuestionsService {
   private readonly apiService = inject(ApiService);
   private readonly errorHandlerService = inject(ErrorHandlerService);
+  private readonly randomizationService = inject(RandomizationService);
 
   getQuestions(amount: number, id: number): Observable<Question[]> {
     return this.apiService.fetchQuestions(id, amount).pipe(
-      map(questions => questions.map(q => this.addAllAnswersToQuestion(q))),
-      catchError((error) => {
-        return this.errorHandlerService.handleError(error);
-      }),
+      map(questions => questions.map(q => this.mergeAndShuffleAnswers(q))),
+      catchError(this.errorHandlerService.handleError),
     );
   }
 
-  private addAllAnswersToQuestion(question: QuestionApiResponse): Question {
+  private mergeAndShuffleAnswers(question: QuestionApiResponse): Question {
+    const answers = [ ...question.incorrect_answers, question.correct_answer ];
     return {
       ...question,
-      answers: [ ...question.incorrect_answers, question.correct_answer ],
+      answers: this.randomizationService.getRandomItems(
+        answers,
+        answers.length,
+      ),
     };
   }
 }
