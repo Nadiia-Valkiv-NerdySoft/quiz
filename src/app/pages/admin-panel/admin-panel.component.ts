ee40e4c81c6828e34f045013481373ece219614c
Nadiia-Valkiv-NerdySoft <nadiia.valkiv@nerdysoft.com>
2025-02-18 23:12:38 +0200

    feat: add add-user and users-list components

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,110 +1,71 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  ViewChild,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { User } from '../../shared/models/user.model';
 import { UserService } from '../../services/user-service/user.service';
-import { FormatDatePipe } from './format-date.pipe';
 import { SvgIconComponent } from 'angular-svg-icon';
-import { TABLE_HEADERS } from './constants/table-headers';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  ReactiveFormsModule,
-  FormsModule,
-} from '@angular/forms';
-import { DatePickerModule } from 'primeng/datepicker';
-import { CalendarModule } from 'primeng/calendar';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'quiz-admin-panel',
-  imports: [
-    FormatDatePipe,
-    SvgIconComponent,
-    ReactiveFormsModule,
-    DatePickerModule,
-    InputTextModule,
-    TableModule,
-    ButtonModule,
-    FormsModule,
-    CalendarModule,
-  ],
+  imports: [ SvgIconComponent, AddUserComponent, UsersListComponent ],
   templateUrl: './admin-panel.component.html',
 })
-export class AdminPanelComponent implements OnInit {
-  private userService = inject(UserService);
-  private fb = inject(FormBuilder);
+export class AdminPanelComponent implements OnInit, OnDestroy {
+  @ViewChild(UsersListComponent) usersListComponent!: UsersListComponent;
 
-  tableHeaders = TABLE_HEADERS;
-  formGroups: { [key: number]: FormGroup } = {};
-  editingUser: { [key: number]: User } = {};
+  private userService = inject(UserService);
+  private subscription: Subscription | null = null;
 
   users = signal<User[]>([]);
   isFormVisible = signal(false);
-  userForm: FormGroup = this.fb.group({
-    first_name: [ '', Validators.required ],
-    last_name: [ '', Validators.required ],
-    email: [ '', [ Validators.required, Validators.email ]],
-    dob: [ '', Validators.required ],
-    interests: [ '', Validators.required ],
-  });
-
-  private isSubmitting = signal(false);
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(users => this.users.set(users));
+    this.loadUsers();
   }
 
-  toggleForm() {
-    this.isFormVisible.update(current => !current);
-    if (!this.isFormVisible()) { this.userForm.reset(); }
-  }
-
-  onSubmit() {
-    if (this.userForm.valid && !this.isSubmitting()) {
-      this.isSubmitting.set(true);
-      const newUser: User = { ...this.userForm.value };
-      this.userService.createUser(newUser).subscribe(
-        (user) => {
-          this.users.update(currentUsers => [ ...currentUsers, user ]);
-          this.toggleForm();
-          this.isSubmitting.set(false);
-        },
-        () => {
-          this.isSubmitting.set(false);
-          this.userForm.reset();
-        },
-      );
-    }
-  }
-
-  deleteUser(id: number, event: Event): void {
-    event.stopPropagation();
-    this.userService.deleteUser(id).subscribe(() => {
-      this.users.update(currentUsers => currentUsers.filter(user => user.id !== id));
-      delete this.formGroups[id];
+  loadUsers(): void {
+    this.subscription = this.userService.getUsers().subscribe({
+      next: users => this.users.set(users),
     });
   }
 
-  onRowEditInit(user: User) {
-    this.editingUser[user.id] = { ...user };
+  toggleForm(): void {
+    this.isFormVisible.update(current => !current);
   }
 
-  onRowEditSave(user: User) {
-    this.userService.updateUser(user).subscribe(
-      (updatedUser) => {
+  onUserSaved(newUser: User) {
+    this.subscription = this.userService.updateUser(newUser).subscribe(() => {
+      this.toggleForm();
+      this.users.update(currentUsers => [ ...currentUsers, newUser ]);
+    });
+  }
+
+  updateUser(user: User): void {
+    this.subscription = this.userService.updateUser(user).subscribe({
+      next: (updatedUser) => {
         this.users.update(currentUsers => currentUsers.map(u => (u.id === updatedUser.id ? updatedUser : u)));
-        delete this.editingUser[user.id];
       },
-      () => this.onRowEditCancel(user),
-    );
+      error: () => this.usersListComponent.onRowEditCancel(user),
+    });
   }
 
-  onRowEditCancel(user: User) {
-    if (this.editingUser[user.id]) {
-      Object.assign(user, this.editingUser[user.id]);
-      delete this.editingUser[user.id];
+  onDeleteUser(id: number): void {
+    this.subscription = this.userService.deleteUser(id).subscribe(() => {
+      this.users.update(currentUsers => currentUsers.filter(user => user.id !== id));
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 }
--- src/app/pages/admin-panel/components/users-list/users-list.component.ts
+++ src/app/pages/admin-panel/components/users-list/users-list.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../../../shared/models/user.model';
+import { FormatDatePipe } from '../../format-date.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TABLE_HEADERS } from '../../constants/table-headers';
 
 @Component({
   selector: 'quiz-users-list',
-  imports: [],
+  imports: [
+    FormatDatePipe,
+    ReactiveFormsModule,
+    DatePickerModule,
+    InputTextModule,
+    TableModule,
+    ButtonModule,
+    FormsModule,
+  ],
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss',
 })
 export class UsersListComponent {
+  @Output() updatedUser = new EventEmitter<User>();
+  @Output() deleteUserId = new EventEmitter<number>();
 
+  @Input() users!: User[];
+
+  tableHeaders = TABLE_HEADERS;
+  editingUser: { [key: number]: User } = {};
+
+  onRowEditInit(user: User): void {
+    this.editingUser[user.id] = { ...user };
+  }
+
+  onRowEditSave(user: User): void {
+    this.updatedUser.emit(user);
+  }
+
+  onRowEditCancel(user: User): void {
+    if (this.editingUser[user.id]) {
+      Object.assign(user, this.editingUser[user.id]);
+      delete this.editingUser[user.id];
+    }
+  }
+
+  deleteUser(id: number): void {
+    this.deleteUserId.emit(id);
+  }
 }
